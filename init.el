ea73ac88d9a527a8ef5465f729fa6c8307116294
Carlos Saito <c.saito.style@gmail.com>
2018-10-04 16:12:42 +0200

    Key bindings

---
+++ init.el
@@ -0,0 +1,3 @@
+(add-to-list 'load-path (expand-file-name "settings" user-emacs-directory))
+
+(require 'keyboard-modifiers)
---
+++ settings/keyboard-modifiers.el
@@ -0,0 +1,10 @@
+;;; Set key bindings for the modifier keys.
+
+;; Use "right alt" for "normal alt"
+;; and "left alt" as "super"
+(setq ns-right-alternate-modifier nil)
+(setq ns-left-alternate-modifier 'super)
+(setq mac-command-modifier 'meta)
+(setq ns-function-modifier 'hyper)
+
+(provide 'keyboard-modifiers)
