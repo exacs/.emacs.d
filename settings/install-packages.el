a505c2613d596e63e466bf7cd3517fd4aac12ed0
Carlos Saito <exacs@users.noreply.github.com>
2018-10-30 15:06:03 +0100

    Install magit & undo-tree

--- settings/install-packages.el
+++ settings/install-packages.el
@@ -1,3 +1,11 @@
 ;;;; Install packages
+(use-package magit
+  :ensure t
+  :bind (("C-x g" . 'magit-status)))
+
+(use-package undo-tree
+  :ensure t
+  :config
+  (global-undo-tree-mode 1))
 
 (provide 'install-packages)
