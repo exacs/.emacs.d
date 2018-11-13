b635ef1798157dfc3f1b62b747b956805a43d2d8
Carlos Saito <exacs@users.noreply.github.com>
2018-11-13 09:25:13 +0100

    Change docs a little bit

--- settings/settings-global.el
+++ settings/settings-global.el
@@ -58,7 +58,7 @@
 (setq line-number-mode t)
 (setq column-number-mode t)
 
-;; Save a list of recent files visited. (open recent file with C-x f)
+;; Save a list of recent files visited.
 (recentf-mode 1)
 (setq recentf-max-saved-items 100) ;; just 20 is too recent
 
